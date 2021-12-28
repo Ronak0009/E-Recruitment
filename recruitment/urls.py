3339d8b9eeaade9474a74271244b253a59eca1ea
Ronak Kanthariya <76777057+Ronak0009@users.noreply.github.com>
2021-12-28 10:50:36 +0530

    Add files via upload

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('common.urls')),
     path('admin/', admin.site.urls),
-    path('employee/',include('employee.urls')),
+    path('candidate/',include('employee.urls')),
+    path('company/',include('company.urls')),
+    path('admins/',include('admins.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
